cece3f210f9c23263620d06929eecbe736996332
mk-saadi <mksaadi820@gmail.com>
2023-10-09 22:00:11 +0600

    moon

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 
 require("dotenv").config();
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 9000;
 
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: "unauthorized access" });
+    }
+    const token = authorization.split(" ")[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: "unauthorized access" });
+        }
+        req.decoded = decoded;
+        next();
+    });
+};
+
 // mongoDB
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mrt0xqs.mongodb.net/?retryWrites=true&w=majority`;
+
+const uri = `${process.env.Mongo_URI}`;
 
 const client = new MongoClient(uri, {
     serverApi: {
@@ -19,32 +37,120 @@ const client = new MongoClient(uri, {
         deprecationErrors: true,
     },
 });
-
+// tz5W1lH05hBSMVmN;
 async function run() {
     try {
-        const figureCollection = client.db("animeFig").collection("figures");
-        const addedFigureCollection = client.db("animeFig").collection("addedFigure");
+        const usersCollection = client.db("moon-task1").collection("users");
+        const jewelryCollection = client.db("moon-task1").collection("jewelries");
+        const addedJewelryCollection = client.db("moon-task1").collection("addedJewelry");
 
-        app.get("/figures", async (req, res) => {
-            const cursor = figureCollection.find();
+        app.post("/jwt", (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: "7d",
+            });
+
+            res.send({ token });
+        });
+
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            if (user?.role !== "admin") {
+                return res.status(403).send({ error: true, message: "forbidden access!" });
+            }
+            next();
+        };
+
+        // users
+        app.get("/users", verifyJWT, verifyAdmin, async (req, res) => {
+            let query = {};
+
+            if (req.query?.email) {
+                query.email = { $regex: req.query.email, $options: "i" };
+            }
+
+            if (req.query?.name) {
+                query.name = { $regex: req.query.name, $options: "i" };
+            }
+
+            if (req.query?.role) {
+                query.role = { $regex: req.query.role, $options: "i" };
+            }
+
+            const result = await usersCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        app.get("/users/:role/:email", verifyJWT, async (req, res) => {
+            const role = req.params.role;
+            const email = req.params.email;
+
+            if (email !== req.decoded.email) {
+                return res.send({ [role]: false });
+            }
+
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            const result = { [role]: user?.role === role };
+            res.send(result);
+        });
+
+        app.patch("/users/:email", verifyJWT, verifyAdmin, async (req, res) => {
+            const { email } = req.params;
+            const { role } = req.body;
+
+            try {
+                await usersCollection.updateOne({ email: email }, { $set: { role: role } });
+
+                res.status(200).send({ message: "User role updated successfully" });
+            } catch (error) {
+                console.error("Error updating user role:", error);
+                res.status(500).send({ error: "Internal server error" });
+            }
+        });
+
+        app.post("/users", async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email };
+            const existingUser = await usersCollection.findOne(query);
+
+            if (existingUser) {
+                return res.send({ message: "user already exists" });
+            }
+
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
+
+        app.delete("/users/:id", verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await usersCollection.deleteOne(query);
+            res.send(result);
+        });
+
+        app.get("/jewelries", async (req, res) => {
+            const cursor = jewelryCollection.find();
             const result = await cursor.toArray();
             res.send(result);
         });
 
-        app.get("/figures/:id", async (req, res) => {
+        app.get("/jewelries/:id", async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
-            const result = await figureCollection.findOne(query);
+            const result = await jewelryCollection.findOne(query);
             res.send(result);
         });
 
-        app.post("/figures", async (req, res) => {
-            const figures = req.body;
-            const result = await figureCollection.insertOne(figures);
+        app.post("/jewelries", async (req, res) => {
+            const jewelries = req.body;
+            const result = await jewelryCollection.insertOne(jewelries);
             res.send(result);
         });
 
-        app.get("/addedFigure", async (req, res) => {
+        app.get("/addedJewelry", async (req, res) => {
             let query = {};
 
             if (req.query?.email) {
@@ -55,81 +161,79 @@ async function run() {
                 query.name = { $regex: req.query.search, $options: "i" };
             }
 
-            if (req.params.category) {
-                if (
-                    req.params.category == "Scale Figures" ||
-                    req.params.category == "Bishoujo Figures" ||
-                    req.params.category == "Figma" ||
-                    req.params.category == "Nendoroid"
-                ) {
-                    query.category = req.params.category;
-                } else {
-                    query.category = { $regex: req.params.category, $options: "i" };
-                }
-            }
+            // if (req.params.category) {
+            //     if (
+            //         req.params.category == "Scale Figures" ||
+            //         req.params.category == "Bishoujo Figures" ||
+            //         req.params.category == "Figma" ||
+            //         req.params.category == "Nendoroid"
+            //     ) {
+            //         query.category = req.params.category;
+            //     } else {
+            //         query.category = { $regex: req.params.category, $options: "i" };
+            //     }
+            // }
 
-            // const limit = parseInt(req.query.limit) || 20;     .limit(limit)
-
-            const result = await addedFigureCollection.find(query).sort({ price: -1 }).toArray();
+            const result = await addedJewelryCollection.find(query).toArray();
 
             res.send(result);
         });
 
-        app.get("/addedFigure/:id", async (req, res) => {
+        app.get("/addedJewelry/:id", async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
-            const result = await addedFigureCollection.findOne(query);
+            const result = await addedJewelryCollection.findOne(query);
             res.send(result);
         });
 
-        app.post("/addedFigure", async (req, res) => {
+        app.post("/addedJewelry", async (req, res) => {
             const figure = req.body;
-            const result = await addedFigureCollection.insertOne(figure);
+            const result = await addedJewelryCollection.insertOne(figure);
             res.send(result);
         });
 
-        app.put("/addedFigure/:id", async (req, res) => {
+        app.put("/addedJewelry/:id", async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) };
             const options = { upsert: true };
 
-            const updatedFigs = req.body;
-            const figures = {
+            const updatedJewel = req.body;
+            const jewelries = {
                 $set: {
-                    img: updatedFigs.img,
-                    email: updatedFigs.email,
-                    name: updatedFigs.name,
-                    price: updatedFigs.price,
-                    quantity: updatedFigs.quantity,
-                    seller: updatedFigs.seller,
-                    description: updatedFigs.description,
-                    Manufacturer: updatedFigs.Manufacturer,
-                    category: updatedFigs.category,
-                    rating: updatedFigs.rating,
+                    img: updatedJewel.img,
+                    email: updatedJewel.email,
+                    name: updatedJewel.name,
+                    price: updatedJewel.price,
+                    quantity: updatedJewel.quantity,
+                    seller: updatedJewel.seller,
+                    description: updatedJewel.description,
+                    Manufacturer: updatedJewel.Manufacturer,
+                    category: updatedJewel.category,
+                    rating: updatedJewel.rating,
                 },
             };
-            const result = await addedFigureCollection.updateOne(filter, figures, options);
+            const result = await addedJewelryCollection.updateOne(filter, jewelries, options);
             res.send(result);
         });
 
-        app.delete("/addedFigure/:id", async (req, res) => {
+        app.delete("/addedJewelry/:id", async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
-            const result = await addedFigureCollection.deleteOne(query);
+            const result = await addedJewelryCollection.deleteOne(query);
             res.send(result);
         });
 
         await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        console.log("Pinged your deployment. You are successfully connected to MongoDB!");
     } finally {
     }
 }
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-    res.send("animeFig server is running");
+    res.send("moon-task1 server is running");
 });
 
 app.listen(port, () => {
-    console.log(`animeFig server is running at ${port}`);
+    console.log(`moon-task1 server is running at ${port}`);
 });
